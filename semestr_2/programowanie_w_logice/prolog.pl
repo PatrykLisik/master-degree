1fe667c8cafe1166eaf3baabb868a75d69cf5d32
Patryk Lisik <lisikpatryk@gmail.com>
2023-03-31 23:18:57 +0200

    programowanie w logice

--- semestr_2/programowanie_w_logice/prolog.pl
+++ semestr_2/programowanie_w_logice/prolog.pl
@@ -31,3 +31,14 @@ babcia(X,Y) :- kobieta(X),matka(X,A),rodzic(A,Y).
 
 nalezy(X, [X|_]) :-  True.
 nalezy(X, [_|Yogon]) :- nalezy(X,Yogon).
+
+usun(X, [X]):-list.
+usun(X, [X|Yogon]):-[Yogon].
+usun(X, [Y|Yogon]):-usun(X,[Y,Yogon]).
+# nie działa - nie można zwacać
+# Type error: `callable' expected, found `[]' (an empty_list)
+
+usun(X, [X], []).
+usun(X,[X|L1], L1).
+usun(X, [Y|L2], [Y|L1]) :- usun(X,L2,L1).
+usun(X,Y) :- usun(X,Y,OUT). # Nie działa zwraca true
