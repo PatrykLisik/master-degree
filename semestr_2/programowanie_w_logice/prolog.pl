dc0f87ad5e1b4247be305c5463063a4f36bad394
Patryk Lisik <lisikpatryk@gmail.com>
2023-04-01 09:37:14 +0100

    Prolog lab

--- semestr_2/programowanie_w_logice/prolog.pl
+++ semestr_2/programowanie_w_logice/prolog.pl
@@ -29,8 +29,9 @@ babcia(X,Y) :- kobieta(X),matka(X,A),rodzic(A,Y).
 
 
 
-nalezy(X, [X|_]) :-  True.
+nalezy(X, [X|_]).
 nalezy(X, [_|Yogon]) :- nalezy(X,Yogon).
+
 
 usun(X, [X]):-list.
 usun(X, [X|Yogon]):-[Yogon].
@@ -42,3 +43,50 @@ usun(X, [X], []).
 usun(X,[X|L1], L1).
 usun(X, [Y|L2], [Y|L1]) :- usun(X,L2,L1).
 usun(X,Y) :- usun(X,Y,OUT). # Nie działa zwraca true
+
+dodaj(X,L1,[X|L1]).
+
+sklej([],L,L).
+sklej([H|T1],L2,[H|T2]):-sklej(T1,L2,T2).
+
+
+usun(X, L, L). # potrzeben jeśli usuwamy element którego nie ma na liście
+usun(X,[X|L1], L1).
+usun(X, [Y|L2], [Y|L1]) :- usun(X,L2,L1).
+
+
+permut([],[]).
+permut(L, [X|P]):- usun(X,L,L1),permut(L1,P).
+
+
+podzbior([],_).
+podzbior([LH|LT],K):-nalezy(LH,K),podzbior(LT,K).
+
+podzbior([a,b],[a,b,c]). #true
+podzbior([a,p],[a,b,c]). # false
+
+
+rozlaczny([],_).
+rozlaczny([LH|LT],K) :- not(nalezy(LH,K)),rozlaczny(LT,K).
+
+rozlaczny([a,p],[a,b,c]). # flase
+rozlaczny([l,p],[a,b,c]). # true
+
+# jeszcze nie działa
+unikalne([],_).
+unikalne([LH|LT],Out):- not(nalezy(LH,Out)),unikalne(LT,[Out|LH]).
+unikalne([LH|LT],Out):- nalezy(LH,Out),unikalne(LT,Out).
+suma(L,K,M) :- unikalne(sklej(L,K),M).
+
+
+
+suma_num3([X],X).
+suma_num3([G|O],M) :- suma_num3(O,N),M is N+G.
+
+
+
+wyst_poz(X,[X|_],N, NCurr):- N==NCurr.
+wyst_poz(X,[_|LT],N,NCurr):- NewCurr is NCurr+1, wyst_poz(X,LT,N,NewCurr).
+wyst_poz(X,[LH|LT],N):- wyst_poz(X,[LH|LT],N,0).
+
+wyst_poz(a,[b,a,d],1). # True
