626bc1d92d499b7a649e30fb485d64a66f387815
Patryk Lisik <lisikpatryk@gmail.com>
2023-06-11 08:09:13 +0100

    kolos mo finalne

--- semestr_2/metody_optymalizacyjne/kolos/tsp.py
+++ semestr_2/metody_optymalizacyjne/kolos/tsp.py
@@ -1,8 +1,14 @@
-import io
-from itertools import permutations, pairwise
+from itertools import permutations, tee
 
 import numpy as np
-from tqdm import tqdm
+
+
+def pairwise(iterable):
+    # pairwise('ABCDEFG') --> AB BC CD DE EF FG
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
 
 if __name__ == '__main__':
     data = np.genfromtxt("./dist_10.txt", delimiter=None, autostrip=True)
@@ -14,7 +20,7 @@ if __name__ == '__main__':
     best_length = np.infty
     # print(f"System file buffer size {io.DEFAULT_BUFFER_SIZE}")
     with open("./tsp_all.txt", "a") as tsp_file:
-        for order in tqdm(permutations(range(length), length)):
+        for order in permutations(range(length), length):
             order = [*order, order[0]]
             road_length = 0
             for point_a, point_b in pairwise(order):
