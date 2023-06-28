6d1e90f0f4fe876a389e8fc146e35222f07abcda
Patryk Lisik <lisikpatryk@gmail.com>
2023-06-28 22:00:26 +0200

    mpk - stop search in api

--- semestr_2/projekt-mpk/src/web/api/frontend_enpoints.py
+++ semestr_2/projekt-mpk/src/web/api/frontend_enpoints.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 import json
+from random import random
 
 from sanic import Blueprint, HTTPResponse, Request
 from sanic.log import logger
+from sanic.response import json as json_response
 from sanic_ext import render
 
 from src.repositories.abstract import AbstractRouteRepository, AbstractTransitRepository
@@ -134,3 +136,96 @@ bus_lines_data = [
 @html_blueprint.route('/bus-lines')
 async def bus_lines(request):
     return await render("show_bus_lines.html", status=200, context={"bus_lines": bus_lines_data})
+
+
+bus_stops_data = [
+    {
+        "id": 1,
+        "name": "Bus Stop 1",
+        "location_x": 10,
+        "location_y": 20,
+        "bus_line_count": 5,
+        "other_stop_connections_count": 3
+    },
+    {
+        "id": 2,
+        "name": "Bus Stop 2",
+        "location_x": 30,
+        "location_y": 40,
+        "bus_line_count": 2,
+        "other_stop_connections_count": 2
+    }
+]
+
+
+@html_blueprint.route('/bus-stops')
+async def bus_stops(request):
+    selected_stop = request.args.get("selected_stop")
+    selected_stop_id = int(selected_stop) if selected_stop else None
+
+    distance_data = None
+    if selected_stop_id:
+        distance_data = [
+            {"stop_id": 2, "stop_name": "Stop 2", "time": 10},
+            {"stop_id": 3, "stop_name": "Stop 3", "time": 15},
+        ]
+
+    logger.info(f"selected_stop {selected_stop_id}")
+    return await render("show_bus_line_stops.html", status=200,
+                        context={"bus_stops": bus_stops_data, "selected_stop_id": selected_stop_id,
+                                 "distances": distance_data})
+
+
+@html_blueprint.route("/api/bus-stops/<stop_id>", methods=["DELETE"])
+async def delete_bus_stop(request, stop_id):
+    global bus_stops_data
+    bus_stops = [stop for stop in bus_stops_data if stop["id"] != int(stop_id)]
+    return json_response({"message": "Bus stop deleted successfully"})
+
+
+# Distance data route
+@html_blueprint.route("/api/bus-stops/<stop_id>/distances")
+async def get_distance_data(request, stop_id):
+    # Example distance data for a stop
+    distance_data = [
+        {"stop_id": 2, "stop_name": "Stop 2", "time": 10},
+        {"stop_id": 3, "stop_name": "Stop 3", "time": 15},
+        {"stop_id": 4, "stop_name": "Stop 4", "time": 20},
+        {"stop_id": 5, "stop_name": "Stop 5", "time": 15},
+        {"stop_id": 6, "stop_name": "Stop 6", "time": 40},
+    ]
+
+    return json_response({"distances": distance_data})
+
+
+# Endpoint to search for stops by name
+@html_blueprint.route("/api/bus-stops/search", methods=["GET"])
+async def search_stops(request):
+    search_text = request.args.get("query")
+
+    # Search for stops by name in your data source
+    # Example: stops = search_stops_by_name(search_text)
+
+    # Return the search results as JSON response
+    stops = [
+        {"id": 1, "name": "Stop A", "location_x": int(random() * 50), "location_y": int(random() * 50), "bus_line_count": int(random() * 20), "other_stop_connections_count":int(random() * 20)},
+        {"id": 2, "name": "Stop B", "location_x": int(random() * 50), "location_y": int(random() * 50), "bus_line_count": int(random() * 20),"other_stop_connections_count":int(random() * 20)},
+        {"id": 3, "name": "Stop C", "location_x": int(random() * 50), "location_y": int(random() * 50), "bus_line_count": int(random() * 20), "other_stop_connections_count":int(random() * 20)}
+    ]
+    return json_response({"stops": stops})
+
+
+@html_blueprint.route("/api/bus-stops/<stop_id>/distances/<other_stop_id>", methods=["PUT"])
+async def save_distance_data(request, stop_id, other_stop_id):
+    time = request.json.get("time")
+    # Save the distance data to the database or perform necessary operations
+
+    return json_response({"message": "Distance data saved successfully"})
+
+
+# Delete distance data route
+@html_blueprint.route("/api/bus-stops/<stop_id>/distances/<other_stop_id>", methods=["DELETE"])
+async def delete_distance_data(request, stop_id, other_stop_id):
+    # Delete the distance data from the database or perform necessary operations
+
+    return json_response({"message": "Distance data deleted successfully"})
