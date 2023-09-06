190a3ba9bbe92dbd597f421e8579536830c29a6f
Patryk Lisik <lisikpatryk@gmail.com>
2023-09-06 19:57:02 +0200

    metody optymalizacji — lepsze tsp

--- semestr_2/metody_optymalizacyjne/kolos/tsp.py
+++ semestr_2/metody_optymalizacyjne/kolos/tsp.py
@@ -1,6 +1,8 @@
+import time
 from itertools import permutations, tee
 
 import numpy as np
+import psutil
 
 
 def pairwise(iterable):
@@ -13,19 +15,23 @@ def pairwise(iterable):
 if __name__ == '__main__':
     data = np.genfromtxt("./dist_10.txt", delimiter=None, autostrip=True)
 
-    length = len(data[0])
-    results_text = {}
-    results = {}
+    # length = len(data[0])
+    length = 10
     best_order = None
+
     best_length = np.infty
     # print(f"System file buffer size {io.DEFAULT_BUFFER_SIZE}")
+
+    with open("./tsp_all.txt", "w") as tsp_file:
+        print("", file=tsp_file)
+    time_start = time.perf_counter()
     with open("./tsp_all.txt", "a") as tsp_file:
-        for order in permutations(range(length), length):
-            order = [*order, order[0]]
+        for order in permutations(np.arange(length), length):
             road_length = 0
             for point_a, point_b in pairwise(order):
                 distance = data[point_a, point_b]
                 road_length += distance
+            road_length += data[order[-1], order[0]]
             print(f"{order}:{road_length}", file=tsp_file, flush=False)
             if road_length < best_length:
                 best_length = road_length
@@ -33,3 +39,6 @@ if __name__ == '__main__':
 
         print(f"\n\nBEST order {best_order}", file=tsp_file)
         print(f"BEST road length {best_length}", file=tsp_file)
+    time_elapsed = (time.perf_counter() - time_start)
+    memMb = psutil.Process().memory_info().rss / 1024.0 / 1024.0
+    print("%5.1f secs %5.1f MByte" % (time_elapsed, memMb))
