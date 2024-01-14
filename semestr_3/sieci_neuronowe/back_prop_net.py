3680c01f55fad2b436099929f1fb448167fb4471
Patryk Lisik <lisikparyk@gmail.com>
2024-01-14 15:55:34 +0100

    bacprop

--- semestr_3/sieci_neuronowe/back_prop_net.py
+++ semestr_3/sieci_neuronowe/back_prop_net.py
@@ -3,17 +3,21 @@ import matplotlib.pyplot as plt
 
 
 neuron_count = 14
+out_neuron_count = 1
+
 
 data = np.array([np.linspace(0, 2 * np.pi, neuron_count)])
-w = np.random.uniform(-1, 1, (data.shape[1], neuron_count))
+h_w = np.random.uniform(-1, 1, (data.shape[1], neuron_count))
+o_w = np.random.uniform(-1, 1, (out_neuron_count, neuron_count))
+
 expected = np.array([[(np.sin(np.linspace(0, 2 * np.pi, neuron_count)) + 1) / 2]])
 
 learn_speed = 0.01
 
 
-def run_neuron(input, weigths, activation_func):
-    sum = np.dot(input, weigths)
-    return activation_func(sum)
+def run_neuron(input, h_weigths, o_weigths, activation_func):
+    h_out = activation_func(np.dot(input, h_weigths))
+    return activation_func(np.dot(o_weigths, h_out))
 
 
 def error(output, expected):
@@ -21,19 +25,25 @@ def error(output, expected):
     return err
 
 
-def learn(inputs, weigths, error, learn_speed, d_activation):
-    new_w = weigths.copy()
-    # error = np.expand_dims(error, axis=1)
-    # inputs = np.expand_dims(inputs, axis=0)
-    print(f"Error {error.shape}")
-    # print(f"Inputs {inputs.shape}")
-    change = learn_speed * np.outer(inputs,
-                                    error*d_activation(np.dot(inputs, weigths))
-                                    )
-    # print("Change")
-    # print(change)
-    new_w = new_w + change
-    return new_w
+def learn(
+    inputs,
+    expected,
+    h_weigths,
+    o_weigths,
+    out_err,
+    learn_speed,
+    d_activation,
+    activation,
+):
+    h_out = activation(np.dot(h_weigths, inputs))
+    o_out = activation(np.dot(o_weigths, h_out))
+
+    o_err = expected - o_out
+    print(f"p_err {o_err}")
+    h_err = np.dot(h_weigths, o_err.T)
+
+    o_weigths += learn_speed * np.dot(o_err * d_activation(o_out), h_out.T)
+    h_weigths += learn_speed * np.dot(h_err * h_out * (1 - h_out), inputs.T)
 
 
 def line_activation(a, b):
@@ -69,7 +79,7 @@ def norm(d):
 afunc = line_activation(a=1, b=0)
 
 epoch = 0
-print(w)
+print(h_w)
 print()
 while True:
     print(f"Epoch {epoch}")
@@ -78,20 +88,25 @@ while True:
     # print(f" dnorm {d_norm}")
 
     for in_, exp_ in zip(d_norm, expected):
-        n_out = run_neuron(weigths=w, input=in_, activation_func=sigmoid)
+        n_out = run_neuron(
+            h_weigths=h_w, o_weigths=o_w, input=in_, activation_func=sigmoid
+        )
         err_ = exp_ - n_out
         print(f"Inp {in_}")
         print(f"Out {n_out}")
         print(f"Exp {exp_}")
         print(f"error to learn {err_}")
-        w = learn(
+        h_w = learn(
             inputs=in_,
-            error=err_,
-            weigths=w,
+            expected=exp_,
+            out_err=err_,
+            h_weigths=h_w,
+            o_weigths=o_w,
             learn_speed=learn_speed,
             d_activation=d_sigmoid,
+            activation=sigmoid
         )
-        print(f"error avg {np.mean(np.abs(err_))} | erro max {np.max(err_)}")
+        print(f"error avg {np.mean(np.abs(err_))} | error max {np.max(np.abs(err_))}")
         # print("New weigths")
         # print(w)
     user_in = input("Press k to plot ")
@@ -100,7 +115,7 @@ while True:
         for drif in [0.01, 0.1, 0.2, 0.3, 0.4]:
             y = np.array([np.linspace(0, 2 * np.pi, 14)]) - drif
             net_out = [
-                run_neuron(weigths=w, input=yy, activation_func=sigmoid) for yy in y
+                run_neuron(h_weigths=h_w, input=yy, activation_func=sigmoid) for yy in y
             ]
             plt.scatter(y, net_out, color="red")
         plt.grid()
